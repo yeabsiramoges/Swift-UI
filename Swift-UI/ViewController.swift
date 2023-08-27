555222853858eb36e63194b109ab659b9056b0f4
yeabsiramoges <yeab@mit.edu>
2023-08-27 11:49:32 -0700

    first commit

---
+++ Swift-UI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift-UI
+//
+//  Created by Yeabsira Moges on 8/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
